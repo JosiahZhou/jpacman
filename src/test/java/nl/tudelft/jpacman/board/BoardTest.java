8879ff3b269b5d3c37bd1908cae9e4ca90045085
Josiah <Josiah@JosiahdeMacBook-Air.local>
2018-05-01 23:16:38 +0200

    Static Analysis Done

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-package nl.tudelft.jpacman.board;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-
-/**
- * Test various aspects of board.
- *
- * @author Jeroen Roosen
- */
-class BoardTest {
-
-    private static final int WIDTH = 1;
-    private static final int HEIGHT = 1;
-
-    private final Square[][] grid = {
-            { mock(Square.class) },
-    };
-    private final Board board = new Board(grid);
-
-    /**
-     * Verifies the board has the correct width.
-     */
-    @Test
-    void verifyWidth() {
-        assertThat(board.getWidth()).isEqualTo(WIDTH);
-    }
-
-    /**
-     * Verifies the board has the correct height.
-     */
-    @Test
-    void verifyHeight() {
-        assertThat(board.getHeight()).isEqualTo(HEIGHT);
-    }
-
-    /**
-     * Verify that squares at key positions are properly set.
-     * @param x Horizontal coordinate of relevant cell.
-     * @param y Vertical coordinate of relevant cell.
-     */
-    @ParameterizedTest
-    @CsvSource({
-            "0, 0"
-    })
-    void testSquareAt(int x, int y) {
-        assertThat(board.squareAt(x, y)).isEqualTo(grid[x][y]);
-        //assertThat(boardNull.squareAt(x, y)).isEqualTo(gridNull[x][y]);
-    }
-}
-=======
 package nl.tudelft.jpacman.board;
 
 import org.junit.jupiter.api.Test;
@@ -108,4 +52,3 @@ class BoardTest {
         assertThat(board.squareAt(x, y)).isEqualTo(grid[x][y]);
     }
 }
->>>>>>> 5fcb5b42e3292beedc995c5d03908289d45f6bec
