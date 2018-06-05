7765e405714cc2507a55c424272a74ffa51dde22
Chenru Lin <C.Lin-2@student.tuelft.nl>
2018-06-05 14:39:43 +0200

    add a test suite for Game.Start() method (Ex 3)

---
+++ src/test/java/nl/tudelft/jpacman/game/GameUnitTest.java
@@ -0,0 +1,66 @@
+package nl.tudelft.jpacman.game;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+/**
+ * @author: Chenru Lin
+ * @discription:
+ */
+public class GameUnitTest {
+    private Game singlePlayerGame;
+    private final Level level = mock(Level.class);
+    private final Player player = mock(Player.class);
+    private final PlayerFactory playerFactory = mock(PlayerFactory.class);
+
+    private final GameFactory gameFactory = new GameFactory(playerFactory);
+
+    @BeforeEach
+    void setUp() {
+        when(playerFactory.createPacMan()).thenReturn(player);
+        singlePlayerGame = gameFactory.createSinglePlayerGame(level);
+    }
+
+    /**
+     * Branch with false -> true.
+     */
+    @Test
+    void gameStart() {
+        when(level.isAnyPlayerAlive()).thenReturn(true);
+        when(level.remainingPellets()).thenReturn(2);
+        singlePlayerGame.start();
+        assertThat(singlePlayerGame.isInProgress()).isTrue();
+    }
+
+    /**
+     * Branch with true -> true.
+     */
+    @Test
+    void InProgress() {
+        when(level.isAnyPlayerAlive()).thenReturn(true);
+        when(level.remainingPellets()).thenReturn(2);
+        singlePlayerGame.start();//change isInProgress to true
+
+        singlePlayerGame.start();
+        assertThat(singlePlayerGame.isInProgress()).isTrue();
+    }
+
+    /**
+     * Branch with false -> false.
+     */
+    @Test
+    void gameOver() {
+        when(level.isAnyPlayerAlive()).thenReturn(false);
+        when(level.remainingPellets()).thenReturn(2);
+        singlePlayerGame.start();
+        assertThat(singlePlayerGame.isInProgress()).isFalse();
+    }
+
+
+
+}
